e1aee8e91394e94fe869ed71b4fa66c8654f6b73
AnKh <43561035+AnKhCan@users.noreply.github.com>
2026-04-23 10:11:42 +0800

    chore: 更新导航数据 - 2026/4/23 10:11:44

--- src/mock/mock_data.js
+++ src/mock/mock_data.js
@@ -10,7 +10,7 @@ export const mockData = {
           "id": "site-1776910204260",
           "name": "HexHub",
           "url": "https://www.hexhub.cn/",
-          "description": "",
+          "description": "全功能 SSH 客户端\n把终端、文件传输和高级连接能力放到一个工作区，适合日常运维与远程开发。",
           "icon": "https://www.hexhub.cn/imgs/logo.png"
         }
       ]
