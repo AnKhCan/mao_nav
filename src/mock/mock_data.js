1222e48432cd439b2e5ed6dd82ac5c1594a81402
AnKh <43561035+AnKhCan@users.noreply.github.com>
2026-06-03 10:31:12 +0800

    chore: 更新导航数据 - 2026/6/3 10:31:11

--- src/mock/mock_data.js
+++ src/mock/mock_data.js
@@ -106,6 +106,13 @@ export const mockData = {
           "url": "http://tinydrive.cn:8096/",
           "description": "",
           "icon": "https://vip.123pan.cn/1779195/yk6baz03t0l000ddwvobo8oucbg70whoDIU0BIe2AGxvBdQ=.png"
+        },
+        {
+          "id": "site-1780453728798",
+          "name": "NAS",
+          "url": "http://tinydrive.cn:19995/#/",
+          "description": "",
+          "icon": ""
         }
       ]
     }
